052015635810a24fc0f8d50039d02464ae39cd98
hari5h <hharishh4@gmail.com>
2026-07-06 16:57:05 -0700

    Add: binary-tree-zigzag-level-order-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-zigzag-level-order-traversal/submission-0.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        q = collections.deque()
+        q.append(root)
+        res = []
+        while q:
+            level = []
+            qlen = len(q)
+            for i in range(qlen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+
+            if level:
+                if len(res) % 2 != 0:
+                    res.append(level[::-1])
+                else:
+                    res.append(level)
+
+        return res
